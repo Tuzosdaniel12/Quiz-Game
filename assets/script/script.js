39c7ed45d8a519d2d9ec2365a37c785d2323e20e
daniel <danielsoledad@gmail.com>
2020-10-28 02:37:16 -0700

    adding display function, check answer function, formatadtime function, and clear children before displaying next quetion

--- assets/script/script.js
+++ assets/script/script.js
@@ -2,59 +2,179 @@ var buttonStart = document.getElementById("start-game");
 var timeLeft = document.getElementById("time-left");
 var questionSection = document.getElementById("question-block");
 var introBlock = document.getElementById("intro-block");
+var answers = document.getElementById("answers");
+var questionEl = document.getElementById("question");
+var correctOrWrongEl = document.getElementById("correctOrWrongAnswer");
+var scoreInfo = document.getElementById("scoreInfo");
 
+var interval;
+var score = 0;
 var totalSeconds = 120;
-var secondPerMinute = 59;
 var currentQuestion = 0;
+var answerList = [];
+var questions = [
+        questionOne = {
+            q : "Whats the most common way to create a variable?",
+            a: ["var","int","float","string"],
+            rightAnswer : 0
+        },
+        questionTwo = {
+            q : "What key word is used to define a function?",
+            a: ["module","var","float","function"],
+            rightAnswer : 3
+        },
+        questionThree = {
+            q : "How do you start a conditional statement?",
+            a: ["else","else if","if","else if"],
+            rightAnswer : 2
+        },
+        questionFour = {
+            q : "what special character do you use to declare an array",
+            a: ["{}","[]","<>","$$"],
+            rightAnswer : 1
+        },
+        questionFive = {
+            q : "how do you set the value of a variable?",
+            a: ["==","===","=","!="],
+            rightAnswer : 2
+        },
+        questionSix = {
+            q : "What tag we use to link a JavaScript file to html",
+            a: ["js","javascript","link","script"],
+            rightAnswer : 3
+        },
+        questionSeven = {
+            q : "whats the correct syntax for a for loop?",
+            a: ["for i = 0, i < 5, i++",
+                "(for i = 0; i < 5; i++)",
+                "for i < 5",
+                "for i to 5"],
+            rightAnswer : 1
+        },
+        questionEight = {
+            q : "What keyword do you use to return value from a function",
+            a: ["return","break","float","string"],
+            rightAnswer : 0
+        },
+        questionNine = {
+            q : "What event is use to listed when the user, uses enter to input value ",
+            a: ["click","hover","dblclick","submit"],
+            rightAnswer : 3
+        },
+        questionTen = {
+            q : "How do you target an elemnt by ID",
+            a: ["document.getElementByClass()","document.getElementById()","document.querySelector()","document.getElementsByTagName"],
+            rightAnswer : 1
+        },
+]
 
 
 // GIVEN I am taking a code quiz
 // WHEN I click the start button
 
 //this event has to fire the time interverval and send me to the question
-// THEN a timer starts and I am presented with a question
-//I need to store time to jSON so all the pages can access the time 
+// THEN a timer starts and I am presented with a question(go to display quiestion)
 function startGame(e){
     e.preventDefault();
     clearInterval(interval);
-    var interval = setInterval(Timer, 1000);
+    interval = setInterval(Timer, 1000);
     introBlock.style.display = "none";
     questionSection.style.display = "block"; 
+    createButtons();
     
 }
 
-function Timer(){
-    
-    var secondsLeft = totalSeconds --;
-    var totalMinutes = Math.floor(secondsLeft/ 60);
-    //to display minutes and seconds I need to acces inner text on time-left
-    //console.log(totalMinutes, secondsLeft);
+//sets time and calls display time to display time
+function Timer(){ 
+    totalSeconds--;
+    var totalMinutes = Math.floor(totalSeconds/ 60);
+    console.log(totalMinutes);
+    var secondsPerMinute = totalSeconds % 60;
+    displayTime(totalMinutes, secondsPerMinute);
+   // console.log(totalMinutes);
 
-    if(secondPerMinute  < 0){
-        secondPerMinute = 59;
-    }
-    else if(totalMinutes == 2){
+}
+
+//formats time as displays time
+function displayTime(totalMinutes, secondsPerMinute){
+   // console.log(totalMinutes, secondPerMinute);
+    if(totalMinutes == 2){
         totalMinutes = 1;
     }
 
-    timeLeft.textContent = totalMinutes + ":" + secondPerMinute;
-    secondPerMinute --;
+    if(secondsPerMinute < 10){
+        timeLeft.textContent = totalMinutes + ":" + "0" + secondsPerMinute;
+    }
+    else{
+        timeLeft.textContent = totalMinutes + ":" + secondsPerMinute;
+    }
 
+    
+}
+//creates four children for each answer in every question
+function createButtons(){
+    questionEl.textContent = questions[currentQuestion].q;
+    for(i =0; i < questions[currentQuestion].a.length; i++){
+        var li = document.createElement("li")
+        li.innerHTML = "<button>" + questions[currentQuestion].a[i] +"</button>";
+        li.dataIndex = i;
+        console.log(li.dataIndex );
+        answers.append(li);
+    }
+     
+}
+//when you click each button is going to call check answer function to check answers
+function answersButtons(e){
+    if(e.target.matches("button")){
+        //everytime a button is clicked check if they reached the end of questions
+        if (currentQuestion === questions.length-1){
+            finalScore();
+        }
+        e.preventDefault(); 
+        //console.log(e.target.parentElement.dataIndex);
+        var index = parseInt(e.target.parentElement.dataIndex)
+        // console.log(index);
+        checkAnswer(index);
+        removeChildrenBeforeNextQuestion();
+        
+       
+    }
 }
 
 
+function checkAnswer(index){
+    if(index === questions[currentQuestion].rightAnswer){
+        correctOrWrongAnswer.textContent = "Corret";
+        score++;
+        
+    }
 
+    else{
+        totalSeconds = totalSeconds - 20;
+        console.log(totalSeconds);
+        if (totalSeconds <= 0){
+            finalScore();
+        }
+    }
+    console.log(score);
+}
+function removeChildrenBeforeNextQuestion(){
+    while(answers.firstChild){
+        answers.removeChild(answers.firstChild);
+    }
+    currentQuestion++;
+    createButtons();
+}
 
-// WHEN I answer a question
-// THEN I am presented with another question
-// WHEN I answer a question incorrectly
-// THEN time is subtracted from the clock
-// WHEN all questions are answered or the timer reaches 0
-// THEN the game is over
-// WHEN the game is over
-// THEN I can save my initials and score
+function finalScore(){
+    totalSeconds = 120;
+    clearInterval(interval);
+    questionSection.style.display = "none";
+    scoreInfo.style.display = "block";
+}
 
 
 
 //event LIStener 
 buttonStart.addEventListener("click",startGame);
+answers.addEventListener("click", answersButtons);
