829b2842f0fffe4517331238396d1d7f5a2bbe35
daniel <danielsoledad@gmail.com>
2020-10-29 03:20:14 -0700

    moved all my questions to another js file so main script is no so long

---
+++ assets/script/question.js
@@ -0,0 +1,65 @@
+var questions = [
+    questionOne = {
+        q : "Whats the most common way to create a variable?",
+        a: ["var","int","float","string"],
+        rightAnswer : 0
+    },
+    questionTwo = {
+        q : "What key word is used to define a function?",
+        a: ["module","var","float","function"],
+        rightAnswer : 3
+    },
+    questionThree = {
+        q : "How do you start a conditional statement?",
+        a: ["else","else if","if","else if"],
+        rightAnswer : 2
+    },
+    questionFour = {
+        q : "what special character do you use to declare an array",
+        a: ["{}","[]","<>","$$"],
+        rightAnswer : 1
+    },
+    questionFive = {
+        q : "how do you set the value of a variable?",
+        a: ["==","===","=","!="],
+        rightAnswer : 2
+    },
+    questionSix = {
+        q : "What tag we use to link a JavaScript file to html",
+        a: ["js","javascript","link","script"],
+        rightAnswer : 3
+    },
+    questionSeven = {
+        q : "whats the correct syntax for a for loop?",
+        a: ["for i = 0, i < 5, i++",
+            "(for i = 0; i < 5; i++)",
+            "for i < 5",
+            "for i to 5"],
+        rightAnswer : 1
+    },
+    questionEight = {
+        q : "What keyword do you use to return value from a function",
+        a: ["return","break","float","string"],
+        rightAnswer : 0
+    },
+    questionNine = {
+        q : "What event is use to listed when the user, uses enter to input value ",
+        a: ["click","hover","dblclick","submit"],
+        rightAnswer : 3
+    },
+    questionTen = {
+        q : "How do you target an elemnt by ID",
+        a: ["document.getElementByClass()","document.getElementById()","document.querySelector()","document.getElementsByTagName"],
+        rightAnswer : 1
+    },
+    questionEl = {
+        q : "Where is the correct place to insert a JavaScript",
+        a: ["head","body","both"],
+        rightAnswer : 1
+    },
+    questionTwe = {
+        q : "The external JavaScript file must contain the <script> tag.",
+        a: ["true","false"],
+        rightAnswer : 0
+    }
+]
